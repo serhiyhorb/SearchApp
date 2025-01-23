7f91d5706273e819bb8946874cdd987b83c6d482
Serhii <serhiyhorb@gmail.com>
2025-01-23 11:30:45 +0200

    Add empty project

---
+++ SearchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchApp
+//
+//  Created by Serhii on 23.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by Serhii on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
